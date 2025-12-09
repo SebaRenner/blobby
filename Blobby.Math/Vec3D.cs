2893bb6b7e45c2f42c3f89c8ed5517782364af12
SebaRenner <sebastian.renner@gmx.ch>
2025-12-09 13:13:07 +0100

    Implemented vec3d with basic arithmetic

---
+++ Blobby.Math/Vec3D.cs
@@ -0,0 +1,32 @@
+﻿namespace Blobby.Math;
+
+public readonly struct Vec3D
+{
+    public float X { get; }
+
+    public float Y { get; }
+
+    public float Z { get; }
+
+    public Vec3D(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Vec3D operator +(Vec3D a, Vec3D b)
+        => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+    public static Vec3D operator -(Vec3D a, Vec3D b)
+        => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    public static Vec3D operator -(Vec3D v)
+        => new(-v.X, -v.Y, -v.Z);
+
+    public static Vec3D operator *(Vec3D v, float scalar)
+        => new(v.X * scalar, v.Y * scalar, v.Z * scalar);
+
+    public static Vec3D operator /(Vec3D v, float scalar)
+        => new(v.X / scalar, v.Y / scalar, v.Z / scalar);
+}
