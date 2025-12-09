2d1f43f9e9c36c67a84df86d053b15dd38165034
SebaRenner <sebastian.renner@gmx.ch>
2025-12-09 17:20:31 +0100

    Added graphics class library and basic window init

--- Blobby.Console/Program.cs
+++ Blobby.Console/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blobby.Graphics;
+using Blobby.Math;
+
+var blobby = UvSphere.Create(50, 2.0f);
+
+var gameWindow = new BlobbyWindow(800, 600);
---
+++ Blobby.Graphics/BlobbyWindow.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+
+namespace Blobby.Graphics;
+
+public class BlobbyWindow
+{
+    public BlobbyWindow(int windowWidth, int windowHeight, int fps = 30)
+    {
+        Raylib.InitWindow(windowWidth, windowHeight, "Blobby");
+
+        Raylib.SetTargetFPS(fps);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
